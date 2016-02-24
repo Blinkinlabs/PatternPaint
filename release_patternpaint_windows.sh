0054d042fb448bdec243cc38d8437c6eefa06d83
Matthew Mets <matt@blinkinlabs.com>
2016-02-25 02:00:37 +0800

    Update Windows release script to generate version from git tag

--- release_patternpaint_windows.sh
+++ release_patternpaint_windows.sh
@@ -40,7 +40,7 @@ OUTDIR='PatternPaintWindows/'
 TEMPDIR='TempWindowsBuild'
 
 
-VERSION='2.0.3'
+VERSION='0.1.0'
 
 
 ################## New directory #################################
@@ -87,6 +87,17 @@ else
 	git pull
 	cd ..
 fi
+
+################### Extract the version info ###################
+cd ${PATTERNPAINT}/PatternPaint
+
+GIT_COMMAND="git -C ${PWD}"
+GIT_VERSION=`${GIT_COMMAND} describe --always --tags 2> /dev/null`
+VERSION=`echo ${GIT_VERSION} | sed 's/-/\./g' | sed 's/g//g'`
+
+echo "PatternPaint version: " ${VERSION}
+
+cd ../../
 
 ################## Build PatternPaint ###################
 cd ${PATTERNPAINT}/PatternPaint
