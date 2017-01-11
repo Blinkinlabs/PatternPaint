70222318fd16306fdb7fcb77cd58bc6979d88a23
Matt Mets <matt@blinkinlabs.com>
2017-01-12 03:39:00 +0800

    Move desktop file and update arduino build scripty

--- buildArduinoFirmware.sh
+++ buildArduinoFirmware.sh
@@ -6,11 +6,11 @@ set -e
 # Note: Use Arduino 1.6.12
 ARDUINO="/Applications/Arduino-1.6.12.app/Contents/MacOS/Arduino"
 BOARD="Blinkinlabs:avr:blinkytape"
-HEX_CONVERTER="PatternPaint/PatternPlayer_Sketch/hex_to_header.py"
+HEX_CONVERTER="src/PatternPlayer_Sketch/hex_to_header.py"
 
 SKETCH_NAME='PatternPlayer_Sketch'
-SKETCH_DIR="$PWD/PatternPaint/PatternPlayer_Sketch"
-OUTPUT_DIR="PatternPaint/controllers"
+SKETCH_DIR="$PWD/src/PatternPlayer_Sketch"
+OUTPUT_DIR="src/libblinky/controllers"
 OUTPUT_NAME="PATTERNPLAYER"
 
 ARDUINO_FLAGS="--verify --verbose --preserve-temp-files --board ${BOARD}"
@@ -28,7 +28,7 @@ ${HEX_CONVERTER} ${HEX} ${OUTPUT_NAME} > ${OUTPUT_DIR}/${SKETCH_NAME}.h
 
 SKETCH_NAME='ProductionSketch'
 SKETCH_DIR="$PWD/..//BlinkyTape_Arduino/examples/ProductionSketch"
-OUTPUT_DIR="PatternPaint/controllers"
+OUTPUT_DIR="src/controllers"
 OUTPUT_NAME="PRODUCTION"
 
 # Compile the sketch, and extract the .hex filename from the output, then convert it to a header
