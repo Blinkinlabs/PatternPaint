c6e530abf21c91701107364b64668c8b47d884ad
Matt Mets <matt@blinkinlabs.com>
2017-01-17 03:26:59 +0800

    Use a shadow directory for cleaner windows build

--- dist_windows.sh
+++ dist_windows.sh
@@ -21,7 +21,6 @@ QT_REDIST=${QT_DIR}'Tools/QtCreator/bin/'
 
 # TODO: Adjust for Win32/64?
 PROGRAMFILES='/c/Program Files (x86)/'
-#PROGRAMFILES='/c/Program Files/'
 
 # Location of the Windows SDK and WDK
 WIN_KIT_SIGNTOOL=${PROGRAMFILES}'Windows Kits/10/'
@@ -30,8 +29,13 @@ WIN_KIT_DPINST=${PROGRAMFILES}'Windows Kits/8.1/'
 # Location of NSIS
 NSIS=${PROGRAMFILES}'NSIS/'
 
-# Location of the PatternPaint repository
-PATTERNPAINT='PatternPaint/'
+# Location to build PatternPaint
+SHADOWBUILD='dist-windows/'
+
+# Staging directory for assembling the installer
+OUTDIR=${SHADOWBUILD}'bin/'
+
+DRIVERREPOS='windows-drivers/'
 
 # Blinkinlabs32u4_boards repository (for the BlinkyTape driver)
 BLINKYTAPE='Blinkinlabs32u4_boards/'
@@ -46,38 +50,33 @@ BLINKYPENDANT='Blinkypendant/'
 EIGHTBYEIGHT='EightByEight/'
 
 # Winsparkle library release
-WINSPARKLE=${PATTERNPAINT}'thirdparty/WinSparkle-0.5.2/'
+WINSPARKLE='thirdparty/WinSparkle-0.5.2/'
 
 # libusb library release
-LIBUSB=${PATTERNPAINT}'thirdparty/libusb-1.0.20-win/'
+LIBUSB='thirdparty/libusb-1.0.20-win/'
 
-# Staging directory for this release
-OUTDIR='PatternPaintWindows/'
-
-TEMPDIR='TempWindowsBuild'
 
 
 VERSION='0.1.0'
 
 
-################## New directory #################################
-if [ ! -d "${TEMPDIR}" ]; then
-	mkdir ${TEMPDIR}
-else
-	echo "Warning: Directory already exists, stale files may be used for build."
-fi
-cd ${TEMPDIR}
+################### Extract the version info ###################
+GIT_COMMAND="git -C ${PWD}"
+GIT_VERSION=`${GIT_COMMAND} describe --always --tags 2> /dev/null`
+VERSION=`echo ${GIT_VERSION} | sed 's/-/\./g' | sed 's/g//g'`
+
+echo "PatternPaint version: " ${VERSION}
 
 
-################## Get PatternPaint ###################
-if [ ! -d "${PATTERNPAINT}" ]; then
-#	git clone https://github.com/Blinkinlabs/PatternPaint.git ${PATTERNPAINT}
-	git clone `pwd`/.. ${PATTERNPAINT}
-else
-	cd ${PATTERNPAINT}
-	git pull
-	cd ..
+
+
+################## Make driver repo directory ####################
+if [ ! -d "${DRIVERREPOS}" ]; then
+	mkdir ${DRIVERREPOS}
 fi
+
+pushd ${DRIVERREPOS}
+
 
 ################## Get BlinkyTape driver #########################
 if [ ! -d "${BLINKYTAPE}" ]; then
@@ -115,28 +114,23 @@ else
 	cd ..
 fi
 
-################### Extract the version info ###################
-pushd ${PATTERNPAINT}
-
-GIT_COMMAND="git -C ${PWD}"
-GIT_VERSION=`${GIT_COMMAND} describe --always --tags 2> /dev/null`
-VERSION=`echo ${GIT_VERSION} | sed 's/-/\./g' | sed 's/g//g'`
-
-echo "PatternPaint version: " ${VERSION}
-
 popd
 
 ################## Build PatternPaint ###################
-pushd ${PATTERNPAINT}src
+if [ ! -d "${SHADOWBUILD}" ]; then
+	mkdir ${SHADOWBUILD}
+fi
+
+pushd ${SHADOWBUILD}
 
 PATH=${QT_TOOLS}:${QT_MINGW}bin/:${PATH}
 
-qmake.exe PatternPaint.pro \
+qmake.exe ../src/PatternPaint.pro \
 	-r \
 	-spec win32-g++ \
 	DESTDIR=release
 	
-mingw32-make.exe clean
+#mingw32-make.exe clean
 mingw32-make.exe -j6
 
 popd
@@ -158,10 +152,10 @@ mkdir -p ${OUTDIR}driver/eightbyeight/x86
 mkdir -p ${OUTDIR}driver/eightbyeight/amd64
 
 # Main executable
-cp ${PATTERNPAINT}src/app/release/PatternPaint.exe ${OUTDIR}
+cp ${SHADOWBUILD}app/release/PatternPaint.exe ${OUTDIR}
 
 # And the libblinky library
-cp ${PATTERNPAINT}src/libblinky/release/libblinky.dll ${OUTDIR}
+cp ${SHADOWBUILD}libblinky/release/libblinky.dll ${OUTDIR}
 
 # Note: This list of DLLs must be determined by hand, using Dependency Walker
 # Also, the .nsi file should be synchronized with this list, otherwise the file
@@ -192,57 +186,60 @@ cp ${WINSPARKLE}/release/WinSparkle.dll ${OUTDIR}
 cp ${LIBUSB}/MinGW32/dll/libusb-1.0.dll ${OUTDIR}
 
 # BlinkyTape Driver files
-cp ${BLINKYTAPE}avr/driver/blinkinlabs.inf ${OUTDIR}driver/blinkytape/
-cp ${BLINKYTAPE}avr/driver/blinkinlabs.cat ${OUTDIR}driver/blinkytape/
+cp ${DRIVERREPOS}${BLINKYTAPE}avr/driver/blinkinlabs.inf ${OUTDIR}driver/blinkytape/
+cp ${DRIVERREPOS}${BLINKYTAPE}avr/driver/blinkinlabs.cat ${OUTDIR}driver/blinkytape/
 
 # BlinkyTile Driver files
-cp ${BLINKYTILE}driver/lightbuddy_serial.inf ${OUTDIR}driver/lightbuddy/
-cp ${BLINKYTILE}driver/lightbuddy_DFU_runtime.inf ${OUTDIR}driver/lightbuddy/
-cp ${BLINKYTILE}driver/lightbuddy_DFU.inf ${OUTDIR}driver/lightbuddy/
-cp ${BLINKYTILE}driver/libusb_device.cat ${OUTDIR}driver/lightbuddy/
-cp ${BLINKYTILE}driver/blinkinlabs.cat ${OUTDIR}driver/lightbuddy/
-cp ${BLINKYTILE}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/lightbuddy/x86/
-cp ${BLINKYTILE}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/lightbuddy/x86/
-cp ${BLINKYTILE}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/lightbuddy/amd64/
-cp ${BLINKYTILE}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/lightbuddy/amd64/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/lightbuddy_serial.inf ${OUTDIR}driver/lightbuddy/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/lightbuddy_DFU_runtime.inf ${OUTDIR}driver/lightbuddy/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/lightbuddy_DFU.inf ${OUTDIR}driver/lightbuddy/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/libusb_device.cat ${OUTDIR}driver/lightbuddy/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/blinkinlabs.cat ${OUTDIR}driver/lightbuddy/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/lightbuddy/x86/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/lightbuddy/x86/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/lightbuddy/amd64/
+cp ${DRIVERREPOS}${BLINKYTILE}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/lightbuddy/amd64/
 
 # BlinkyPendant Driver files
-cp ${BLINKYPENDANT}driver/blinkypendant_serial.inf ${OUTDIR}driver/blinkypendant/
-cp ${BLINKYPENDANT}driver/blinkypendant_DFU_runtime.inf ${OUTDIR}driver/blinkypendant/
-cp ${BLINKYPENDANT}driver/blinkypendant_DFU.inf ${OUTDIR}driver/blinkypendant/
-cp ${BLINKYPENDANT}driver/libusb_device.cat ${OUTDIR}driver/blinkypendant/
-cp ${BLINKYPENDANT}driver/blinkinlabs.cat ${OUTDIR}driver/blinkypendant/
-cp ${BLINKYPENDANT}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/blinkypendant/x86/
-cp ${BLINKYPENDANT}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/blinkypendant/x86/
-cp ${BLINKYPENDANT}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/blinkypendant/amd64/
-cp ${BLINKYPENDANT}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/blinkypendant/amd64/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/blinkypendant_serial.inf ${OUTDIR}driver/blinkypendant/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/blinkypendant_DFU_runtime.inf ${OUTDIR}driver/blinkypendant/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/blinkypendant_DFU.inf ${OUTDIR}driver/blinkypendant/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/libusb_device.cat ${OUTDIR}driver/blinkypendant/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/blinkinlabs.cat ${OUTDIR}driver/blinkypendant/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/blinkypendant/x86/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/blinkypendant/x86/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/blinkypendant/amd64/
+cp ${DRIVERREPOS}${BLINKYPENDANT}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/blinkypendant/amd64/
 
 # EightByEight Driver files
-cp ${EIGHTBYEIGHT}driver/eightbyeight_serial.inf ${OUTDIR}driver/eightbyeight/
-cp ${EIGHTBYEIGHT}driver/eightbyeight_DFU_runtime.inf ${OUTDIR}driver/eightbyeight/
-cp ${EIGHTBYEIGHT}driver/eightbyeight_DFU.inf ${OUTDIR}driver/eightbyeight/
-cp ${EIGHTBYEIGHT}driver/libusb_device.cat ${OUTDIR}driver/eightbyeight/
-cp ${EIGHTBYEIGHT}driver/blinkinlabs.cat ${OUTDIR}driver/eightbyeight/
-cp ${EIGHTBYEIGHT}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/eightbyeight/x86/
-cp ${EIGHTBYEIGHT}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/eightbyeight/x86/
-cp ${EIGHTBYEIGHT}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/eightbyeight/amd64/
-cp ${EIGHTBYEIGHT}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/eightbyeight/amd64/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/eightbyeight_serial.inf ${OUTDIR}driver/eightbyeight/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/eightbyeight_DFU_runtime.inf ${OUTDIR}driver/eightbyeight/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/eightbyeight_DFU.inf ${OUTDIR}driver/eightbyeight/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/libusb_device.cat ${OUTDIR}driver/eightbyeight/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/blinkinlabs.cat ${OUTDIR}driver/eightbyeight/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/x86/winusbcoinstaller2.dll ${OUTDIR}driver/eightbyeight/x86/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/x86/WdfCoInstaller01009.dll ${OUTDIR}driver/eightbyeight/x86/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/amd64/winusbcoinstaller2.dll ${OUTDIR}driver/eightbyeight/amd64/
+cp ${DRIVERREPOS}${EIGHTBYEIGHT}driver/amd64/WdfCoInstaller01009.dll ${OUTDIR}driver/eightbyeight/amd64/
 
 # Driver installer
 cp "${WIN_KIT_DPINST}redist/DIFx/dpinst/MultiLin/x86/dpinst.exe" ${OUTDIR}driver/dpinst32.exe
 cp "${WIN_KIT_DPINST}redist/DIFx/dpinst/MultiLin/x64/dpinst.exe" ${OUTDIR}driver/dpinst64.exe
 
+
 # Run NSIS to make an executablels
 # For some reason the NSIS file is run from the directory it's located in?
-cp ${PATTERNPAINT}"Pattern Paint.nsi" "Pattern Paint.nsi"
+pushd ${SHADOWBUILD}
+
+cp ../"patternpaint.nsi" "patternpaint.nsi"
 
 # Update the version info in the NSI, fail if it didn't change
-sed -i 's/VERSION_STRING/'${VERSION}'.0/g' "Pattern Paint.nsi"
-grep -q ${VERSION} "Pattern Paint.nsi"
+sed -i 's/VERSION_STRING/'${VERSION}'.0/g' "patternpaint.nsi"
+grep -q ${VERSION} "patternpaint.nsi"
 
-"${NSIS}makensis.exe" "Pattern Paint.nsi"
+"${NSIS}makensis.exe" "patternpaint.nsi"
 
-rm "Pattern Paint.nsi"
+rm "patternpaint.nsi"
 
 if [ -z "$SIGNING_ID" ]; then
     echo "WARNING: Signing ID not found, skipping code signature phase. Resulting binary will not be signed."
@@ -252,3 +249,5 @@ else
 fi
 
 mv "PatternPaint Windows Installer.exe" "../PatternPaint_Installer_"${VERSION}".exe"
+
+popd
